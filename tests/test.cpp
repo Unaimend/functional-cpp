fd7e7c07784e8a84fad52272de5708ab566be1fd
Thomas Dost <dostthomas@icloud.com>
2019-10-01 19:50:36 +0200

    ..

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,42 +9,50 @@ TEST_CASE("map", "[FUNTIONS]")
 {
 
 }
-
-TEST_CASE("Empty List as length 0", "[LIST]")
+TEST_CASE("Construction", "[LIST")
 {
-    List<int> emptyList;
-    REQUIRE(emptyList.length() == 0);
-}
+        SECTION("Empty List as length 0", "[LIST]")
+        {
+                List<int> emptyList;
+                REQUIRE(emptyList.length() == 0);
+        }
 
 
-TEST_CASE("List constructor correctly increments length(1)", "[LIST]")
-{
-    List<int> listWithOneElement{{1}};
-    REQUIRE(listWithOneElement.length() == 1);
-}
+        SECTION("Empty init. List works with List" , "[LIST]")
+        {
+                List<int> emptyList{{}};
+                REQUIRE(emptyList.length() == 0);
+        }
 
-TEST_CASE("List constructor correctly increments length(3)", "[LIST]")
-{
-    List<int> listWithThreeElements{{1,2,3}};
-    REQUIRE(listWithThreeElements.length() == 3);
-}
+        SECTION("List constructor correctly increments length(1)", "[LIST]")
+        {
+                List<int> listWithOneElement{{1}};
+                REQUIRE(listWithOneElement.length() == 1);
+        }
+        
+        SECTION("List constructor correctly increments length(3)", "[LIST]")
+        {
+                List<int> listWithThreeElements{{1,2,3}};
+                REQUIRE(listWithThreeElements.length() == 3);
+        }
 
 
-TEST_CASE("List constructor correctly inserts 1 elements", "[LIST]")
-{
-    List<int> listWithOne{{1}};
-    REQUIRE(listWithOne.length() == 1);
-    REQUIRE(listWithOne[0] == 1);
-}
+        SECTION("List constructor correctly inserts 1 elements", "[LIST]")
+        {
+                List<int> listWithOne{{1}};
+                REQUIRE(listWithOne.length() == 1);
+                REQUIRE(listWithOne[0] == 1);
+        }
 
-TEST_CASE("List constructor correctly inserts 3 elements\n and subscript return the correct values", "[LIST]")
-{
-    List<int> listWithOneTwoThree{{1,2,3}};
-    REQUIRE(listWithOneTwoThree.length() == 3);
-
-    REQUIRE(listWithOneTwoThree[0] == 1);
-    REQUIRE(listWithOneTwoThree[1] == 2);
-    REQUIRE(listWithOneTwoThree[2] == 3);
+        SECTION("List constructor correctly inserts 3 elements\n and subscript return the correct values", "[LIST]")
+        {
+                List<int> listWithOneTwoThree{{1,2,3}};
+                REQUIRE(listWithOneTwoThree.length() == 3);
+                
+                REQUIRE(listWithOneTwoThree[0] == 1);
+                REQUIRE(listWithOneTwoThree[1] == 2);
+                REQUIRE(listWithOneTwoThree[2] == 3);
+        }
 }
 
 TEST_CASE("Out of bounds subscript throws an exception", "[LIST]")
@@ -97,8 +105,8 @@ TEST_CASE("push_back" , "[LIST]")
         REQUIRE(listWithOneTwoThree.length() == 3);
         REQUIRE(listWithOneTwoThreeFour.length() == 4);
 
-        REQUIRE((listWithOneTwoThree == List<int>{{1,2,3}}));
-        REQUIRE((listWithOneTwoThreeFour == List<int>{{1,2,3,4}}));
+        REQUIRE((listWithOneTwoThree == List<int>{1,2,3}));
+        REQUIRE((listWithOneTwoThreeFour == List<int>{1,2,3,4}));
     }
 
     SECTION("push_back on empty list")
@@ -122,8 +130,8 @@ TEST_CASE("Push_front", "[LIST]")
         REQUIRE(listWithOneTwoThree.length() == 3);
         REQUIRE(listWithFourOneTwoThree.length() == 4);
 
-        REQUIRE((listWithOneTwoThree == List<int>{{1,2,3}}));
-        REQUIRE((listWithFourOneTwoThree== List<int>{{4,1,2,3}}));
+        REQUIRE((listWithOneTwoThree == List<int>{1,2,3}));
+        REQUIRE((listWithFourOneTwoThree== List<int>{4,1,2,3}));
     }
 
     SECTION("push_front on empty list")
@@ -133,5 +141,44 @@ TEST_CASE("Push_front", "[LIST]")
 
         REQUIRE(listWithOne.length() == 1);
         REQUIRE((listWithOne == List<int>{{1}}));
+    }
+
+}
+
+
+TEST_CASE("head", "[LIST]")
+{
+    SECTION("Empty list")
+    {
+         List<int> emptyList{};
+
+         REQUIRE(emptyList.head() == std::nullopt);
+    }
+
+
+    SECTION("Non-empty list")
+    {
+         List<int> emptyList{1,2,3};
+
+         REQUIRE(emptyList.head() == 1);
+    }
+}
+
+
+TEST_CASE("tail", "[LIST]")
+{
+    SECTION("Empty list")
+    {
+         List<int> emptyList{};
+
+         REQUIRE(emptyList.tail() == std::nullopt);
+    }
+
+
+    SECTION("Non-empty list")
+    {
+         List<int> emptyList{1,2,3};
+
+         REQUIRE(emptyList.tail() == 3);
     }
 }
---
+++ tests/tree_test.cpp
@@ -0,0 +1,18 @@
+
+
+#include "../external/Catch2/single_include/catch2/catch.hpp"
+#include "../include/Functional.hpp"
+using namespace func::pure;
+TEST_CASE("Tree", "[BST]")
+{
+    SECTION("Tree creation")
+    {
+        BST_IT Tree{1,2,3,99,54};
+
+        REQUIRE(Tree.member(1));
+        REQUIRE(Tree.member(2));
+        REQUIRE(Tree.member(3));
+        REQUIRE(Tree.member(99));
+        REQUIRE(Tree.member(54));
+    }
+}
